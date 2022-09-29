4d9f247b2fe6519a660c629b9bb77e5266aab77a
KylianB <kylian.balde@gmail.com>
2022-09-29 14:58:47 +0200

    shield belt fix

---
+++ Source/VFEI/Comps/ItemComps/CompShieldNoRecharge.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace VFEI
+{
+    public class CompShieldNoRecharge : CompShield
+    {
+        public bool canRecharge = true;
+
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+            Scribe_Values.Look(ref canRecharge, "canRecharge");
+        }
+
+        public override void CompTick()
+        {
+            if (canRecharge && parent is Apparel apparel && apparel.Wearer != null)
+            {
+                AccessTools.Field(typeof(CompShield), "energy").SetValue(this, parent.GetStatValue(StatDefOf.EnergyShieldEnergyMax));
+                canRecharge = false;
+            }
+        }
+    }
+}
